bdc10377265b2ee85b29bd1ec5318822f655a510
web-dave <dave@webdave.de>
2021-09-11 22:32:56 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'st-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sim-tower';
+  @ViewChild('gameboard') gameboard!: HTMLDivElement;
+  total_floors = new Array(16);
+  total_floor_nodes = new Array(30);
+  cursor_style = 'url("assets/img/icon/pointer.png"), pointer';
+  constructor() {}
+
+  updatePointer(icon: string) {
+    this.cursor_style = `url('assets/img/icon/${icon}.png'), pointer`;
+    console.log(icon, this.cursor_style);
+  }
+  build(node: string) {}
 }
