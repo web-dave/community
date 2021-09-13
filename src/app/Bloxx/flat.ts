d58c54fa361f666ba2bd1699c49584bc47a4504a
web-dave <dave@webdave.de>
2021-09-13 22:02:44 +0200

    #2 population

--- src/app/Bloxx/flat.ts
+++ src/app/Bloxx/flat.ts
@@ -1,10 +1,17 @@
 import { INode } from 'models/node.interface';
+import { first } from 'rxjs/operators';
 import { BankService } from '../bank.service';
 import { GameService } from '../game.service';
+import { takeEveryNth } from '../utils/operators';
 
 export class Flat {
   public readonly type = 'flat';
   private rent = 500;
+
+  adults = 2;
+  kids = 0;
+  jobs = 0;
+
   constructor(
     public node: INode,
     private dom: HTMLDivElement,
@@ -13,6 +20,9 @@ export class Flat {
   ) {
     this.dom.style.backgroundImage = `url("assets/img/icon/unit.png"), url("assets/img/icon/${this.type}.png")`;
     this.bank.subtractByBlock(this.type);
-    engine.rentTick$.subscribe(() => this.bank.subtract(this.rent));
+    this.engine.rentTick$.subscribe(() => this.bank.subtract(this.rent));
+    this.engine.rentTick$.pipe(takeEveryNth(7), first()).subscribe(()=>{
+      this.kids = Math.floor(Math.random() * 4);
+    })
   }
 }
